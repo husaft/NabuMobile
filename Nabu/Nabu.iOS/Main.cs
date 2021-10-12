d0b43507d779cb146a486282032823dcf3c7afdc
Hubertus Sack <hubertussack@outlook.com>
2021-10-12 14:19:17 +0200

    Adding default icons and such

---
+++ Nabu/Nabu.iOS/Main.cs
@@ -0,0 +1,12 @@
+﻿using UIKit;
+
+namespace Nabu.iOS
+{
+    public class Application
+    {
+		private static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
