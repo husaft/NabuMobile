fdd9ec6622e983484112dc51932612056337dc9c
Hubertus Sack <hubertussack@outlook.com>
2021-10-16 20:52:31 +0200

    Building view

--- Nabu/Nabu/ViewModels/SessionViewModel.cs
+++ Nabu/Nabu/ViewModels/SessionViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Nabu.Models;
 using Nabu.Services;
@@ -18,9 +19,16 @@ namespace Nabu.ViewModels
 		public bool PlaySound { get; set; } = true;
 		public IDictionary<string, int> Repetitions { get; set; }
 		public Word[] LectionWords { get; set; }
+		private Random _random;
+
+		public bool IsMode1 => Mode.Id == 0;
+		public bool IsMode2 => Mode.Id == 1;
+		public bool IsMode3 => Mode.Id == 2;
+
 
 		public SessionViewModel()
 		{
+			_random = new Random();
 			Title = "Session";
 			Repetitions = new SortedDictionary<string, int>();
 		}
