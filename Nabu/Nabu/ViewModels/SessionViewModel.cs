272e3dbe61f666e8ddf582392b2d5e1511d33810
Hubertus Sack <hubertussack@outlook.com>
2021-10-16 21:54:25 +0200

    Added ok and solve actions

--- Nabu/Nabu/ViewModels/SessionViewModel.cs
+++ Nabu/Nabu/ViewModels/SessionViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Nabu.Models;
 using Nabu.Services;
+using Xamarin.Forms;
 
 namespace Nabu.ViewModels
 {
@@ -25,12 +26,22 @@ namespace Nabu.ViewModels
 		public bool IsMode2 => Mode.Id == 1;
 		public bool IsMode3 => Mode.Id == 2;
 
+		public string CurrentInput
+		{
+			get => _currentInput; set { _currentInput = value; OnPropertyChanged(); }
+		}
+
+		public Command OkCommand { get; }
+		public Command SolveCommand { get; }
 
 		public SessionViewModel()
 		{
 			_random = new Random();
 			Title = "Session";
 			Repetitions = new SortedDictionary<string, int>();
+
+			OkCommand = new Command(OnOk);
+			SolveCommand = new Command(OnSolve);
 		}
 
 		public void OnAppearing()
@@ -48,6 +59,51 @@ namespace Nabu.ViewModels
 				LectionWords = possible;
 			}
 			IsBusy = false;
+		}
+
+		private static bool CompareText(string first, string second)
+		{
+			first = first?.Trim('/').Trim();
+			second = second?.Trim('/').Trim();
+			if (first == second)
+				return true;
+			if (StringComparer.InvariantCultureIgnoreCase.Compare(first, second) == 0)
+				return true;
+			return false;
+		}
+
+		private int WrongCount;
+		private int CorrectCount;
+		private string _currentInput;
+
+		private void OnOk()
+		{
+			bool isCorrect;
+			if (IsMode1)
+				isCorrect = CompareText(CurrentWord.Language2, CurrentInput);
+			else if (IsMode2)
+				isCorrect = CompareText(CurrentWord.Language1, CurrentInput);
+			else if (IsMode3)
+				isCorrect = CompareText(CurrentWord.Transcription, CurrentInput);
+			else
+				return;
+			if (isCorrect)
+				CorrectCount++;
+			else
+				WrongCount++;
+		}
+
+		private void OnSolve()
+		{
+			if (IsMode1)
+				CurrentInput = CurrentWord.Language2;
+			else if (IsMode2)
+				CurrentInput = CurrentWord.Language1;
+			else if (IsMode3)
+				CurrentInput = CurrentWord.Transcription;
+			else
+				return;
+			WrongCount++;
 		}
 	}
 }
